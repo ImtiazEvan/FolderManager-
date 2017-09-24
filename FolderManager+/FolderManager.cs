97a01809255c95100c18f48c06dd2273b0c249df
EVAN-PC\Evan_2 <evan@ice9interactive.com>
2017-09-24 21:19:42 +0600

    Initial Commit

---
+++ FolderManager+/FolderManager.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class FolderManager : EditorWindow 
+{
+
+    private string directoryPath;
+    private Object activeObj;
+
+    private int numberOfDirectories;
+    private int numberOfFiles;
+
+    struct fileStructure
+    {
+        public fileStructure(int x, int y) 
+        {
+            this.NumberOfFiles = x;
+            this.NumberOfDirectory = y;
+
+        }
+
+        public int NumberOfFiles;
+        public int NumberOfDirectory;
+    }
+
+
+    [MenuItem("Window/Folder Manager")]
+    public static void showWindow() 
+    {
+        EditorWindow.GetWindow(typeof(FolderManager));
+    }
+
+    void Awake() 
+    {
+        numberOfDirectories = 0;
+        numberOfFiles = 0;
+        directoryPath = "";
+
+
+        var objects = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+
+        if(objects.Length > 0) 
+        {
+
+            activeObj = objects[0];
+
+
+            string currentdirectoryPath = AssetDatabase.GetAssetPath(activeObj.GetInstanceID());
+
+
+            if (Directory.Exists(currentdirectoryPath)) {
+                directoryPath = currentdirectoryPath;
+            }
+            else {
+                directoryPath = Path.GetDirectoryName(currentdirectoryPath);
+            }
+
+            fileStructure fs = getFilesAndDirectiories(directoryPath);
+
+            numberOfDirectories = fs.NumberOfDirectory;
+            numberOfFiles = fs.NumberOfFiles;
+
+        }
+        
+    }
+
+
+    void OnGUI()
+    {
+        /*Object obj = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets)[0];
+        string currentdirectoryPath = AssetDatabase.GetAssetPath(obj.GetInstanceID());*/                
+
+        EditorGUI.TextField(new Rect(5,25,position.width-10,16), "Current Directory", directoryPath);
+        EditorGUI.HelpBox(new Rect(5, 45, position.width - 10, 16), "has " + numberOfDirectories + " directories & " + numberOfFiles + " files",MessageType.None);
+
+        if(GUI.Button(new Rect(5,65,position.width/2-10,20),"UnWrap Files")) 
+        {
+            unWrapFolders();
+        }
+
+        if (GUI.Button(new Rect(position.width / 2 + 5, 65, position.width / 2 - 10, 20), "Init Directories")) 
+        {
+            Debug.Log("hello");
+        }
+
+
+    }
+
+    void Update() 
+    {
+
+        var objects = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+
+        if(objects.Length > 0) 
+        {
+
+            Object selectedObj = objects[0];
+
+            if (activeObj != selectedObj) {
+                activeObj = selectedObj;
+
+                string currentdirectoryPath = AssetDatabase.GetAssetPath(activeObj.GetInstanceID());
+
+                if (Directory.Exists(currentdirectoryPath)) {
+                    directoryPath = currentdirectoryPath;
+                }
+                else {
+                    directoryPath = Path.GetDirectoryName(currentdirectoryPath);
+                }
+
+
+                fileStructure fs = getFilesAndDirectiories(directoryPath);
+
+                numberOfDirectories = fs.NumberOfDirectory;
+                numberOfFiles = fs.NumberOfFiles;
+
+            }
+
+            Repaint();
+        }       
+        
+
+    }
+
+
+
+    private static fileStructure getFilesAndDirectiories(string path)
+    {
+        int files = 0;
+        int directories = 0;
+
+
+        Queue<string> t = new Queue<string>();
+        t.Enqueue(path);
+
+
+        while(t.Count > 0) 
+        {
+            string p = t.Dequeue();
+
+            foreach (string fileName in Directory.GetFiles(p)) 
+            {
+                if (!fileName.Contains("meta")) 
+                {
+                    files++;
+                }
+            }
+
+
+            foreach(string subDirectory in Directory.GetDirectories(p)) 
+            {
+
+                directories++;
+                t.Enqueue(subDirectory);
+            }
+        }
+
+        return new fileStructure(files,directories);
+    }
+
+
+    private void unWrapFolders() 
+    {
+        FolderManagerController.UnWrapSubFolders(directoryPath);
+    }
+
+}
---
+++ FolderManager+/FolderManagerController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+using UnityEditor;
+
+public class FolderManagerController : MonoBehaviour 
+{
+
+
+     public static void UnWrapSubFolders(string directoryPath) 
+     {
+
+            float progress = 0.0f;
+            EditorUtility.DisplayProgressBar("Unwraping Folders", "Working ...", progress);
+
+            string[] allFilesPath = getAllFilesAtPath(directoryPath);
+
+            if (allFilesPath.Length > 0) 
+            {
+                for (int i = 0; i < allFilesPath.Length; i++)
+                {
+                    string p = allFilesPath[i];
+
+                    if(!p.Contains("meta") && AssetDatabase.ValidateMoveAsset(p, directoryPath + "/" + Path.GetFileName(p)).Length == 0) 
+                    {
+                        string res = AssetDatabase.MoveAsset(p, directoryPath + "/" + Path.GetFileName(p));
+                        Debug.Log(res);
+                    }                                       
+
+                    progress = (float)i /(float)allFilesPath.Length * 0.8f;                    
+
+                    EditorUtility.DisplayProgressBar("Unwraping Folders", "Working " + (int)Mathf.Ceil(progress * 100.0f) + "%", progress);                    
+
+                }
+
+            }
+
+
+
+            string[] folderPaths = Directory.GetDirectories(directoryPath);
+
+            for (int i = 0; i < folderPaths.Length; i++) 
+            {
+                if (folderPaths[i].Contains("Duplicates"))
+                    continue;
+
+                if (!directoryIsEmpty(folderPaths[i])) 
+                {
+
+
+                    if (!Directory.Exists(Path.Combine(directoryPath, "Duplicates"))) {
+                        AssetDatabase.CreateFolder(directoryPath, "Duplicates");
+                    }
+
+                    string[] allfilesInFolder = Directory.GetFiles(folderPaths[i]);
+
+
+                    if (!Directory.Exists(Path.Combine(directoryPath, "Duplicates/" + Path.GetFileName(folderPaths[i])))) {
+                        AssetDatabase.CreateFolder(Path.Combine(directoryPath, "Duplicates"), Path.GetFileName(folderPaths[i]));
+                    }
+
+
+                    foreach (string subfolderFilePath in allfilesInFolder) 
+                    {
+                        if (!subfolderFilePath.Contains("meta")) 
+                        {
+                            string res = AssetDatabase.MoveAsset(subfolderFilePath, Path.Combine(directoryPath, "Duplicates/" + Path.GetFileName(folderPaths[i])) + "/" + Path.GetFileName(subfolderFilePath));
+                            Debug.Log(res);
+                        }                                                                        
+                    }
+
+                }
+
+                AssetDatabase.MoveAssetToTrash(folderPaths[i]);
+
+                progress = 0.8f + (float)i / folderPaths.Length * 0.2f;
+
+                EditorUtility.DisplayProgressBar("Deleting Folders", "Working " + (int)Mathf.Ceil(progress * 100.0f) + "%", progress);
+            }
+
+            
+            EditorUtility.ClearProgressBar();
+            AssetDatabase.Refresh();        
+    }
+
+
+
+    private static string[] getAllFilesAtPath(string path) 
+    {
+
+        Queue<string> directories = new Queue<string>();
+        List<string> finalPaths = new List<string>();
+
+        directories.Enqueue(path);
+
+
+        while (directories.Count > 0) 
+        {
+            string p = directories.Dequeue();
+
+            foreach (string subDirectory in Directory.GetDirectories(p)) 
+            {
+                directories.Enqueue(subDirectory);
+            }
+
+            if (p != path) 
+            {
+                finalPaths.AddRange(Directory.GetFiles(p));
+            }
+            
+        }        
+
+        return finalPaths.ToArray();
+
+    }
+
+    private static bool directoryIsEmpty(string path) 
+    {
+        Queue<string> t = new Queue<string>();
+
+        t.Enqueue(path);
+
+
+        while(t.Count > 0) 
+        {
+            string p = t.Dequeue();
+
+            string[] paths = Directory.GetFiles(p);
+
+            foreach (string filePath in paths) 
+           {
+                if (!filePath.Contains("meta")) 
+                {
+                    return false;
+                }
+
+            }
+
+            foreach (string subDirectory in Directory.GetDirectories(p)) 
+            {
+                t.Enqueue(subDirectory);            
+            }
+
+        }
+        
+
+        return true;
+    }
+
+
+
+    private static void printArray(string[] res) 
+    {
+        foreach (string p in res) 
+        {
+            Debug.Log(p);
+        }
+    }
+}
